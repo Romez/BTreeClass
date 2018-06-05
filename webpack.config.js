a9e945ab7f590f8f72d172000130070198a17341
roman.ushakov <roman.ushakov.89@inbox.ru>
2018-06-05 11:26:26 +0300

    add prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './examples/test.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
